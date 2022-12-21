c7a0feb897e7b2dfea6e4e5a164f98e343742ccf
abhi16180 <4ni19cs003_a@nie.ac.in>
2022-12-22 00:05:36 +0530

    Update endpoints Fixes #66

--- lib/services/audio_services.dart
+++ lib/services/audio_services.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:easy_localization/easy_localization.dart';
+import 'package:hive/hive.dart';
 import 'package:mime/mime.dart';
+import 'package:o_health/models/audio_resp.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
 import 'package:http/http.dart' as http;
@@ -27,7 +31,9 @@ class AudioServices {
     return savePath;
   }
 
-  sendAudioFile(File waveFile) async {
+  sendAudioFile(File waveFile, String localeCode) async {
+    Box box = Hive.box('data');
+    String userAadharNumber = box.get('userData')['aadhar'];
     var uri = Uri.parse('https://health-conscious.in/api/voicesearch/data');
     final mimeTypeData =
         lookupMimeType(waveFile.path, headerBytes: [0xFF, 0xD8])!.split('/');
@@ -36,6 +42,8 @@ class AudioServices {
     final audioUploadRequest = http.MultipartRequest('POST', uri);
     audioUploadRequest.fields['directoryName'] =
         waveFile.path.split(Platform.pathSeparator).last;
+    audioUploadRequest.fields['user_aadhar_number'] = userAadharNumber;
+    audioUploadRequest.fields['language'] = localeCode;
 
     // Attach the file in the request
     final file = await http.MultipartFile.fromPath('audio-file', waveFile.path,
@@ -48,7 +56,7 @@ class AudioServices {
     try {
       final streamedResponse = await audioUploadRequest.send();
       final response = await http.Response.fromStream(streamedResponse);
-      return response.body;
+      return jsonDecode(response.body);
     } catch (err) {
       return null;
     }
